e37656b07818e7e09e288498c690799351591a1c
Andy Park <andy@bigbearlabs.com>
2016-04-15 19:32:17 +0200

    Initial Commit

---
+++ BBLAccessibility/BBLAccessibility.h
@@ -0,0 +1,19 @@
+//
+//  BBLAccessibility.h
+//  BBLAccessibility
+//
+//  Created by ilo on 15/04/2016.
+//  Copyright © 2016 Big Bear Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BBLAccessibility.
+FOUNDATION_EXPORT double BBLAccessibilityVersionNumber;
+
+//! Project version string for BBLAccessibility.
+FOUNDATION_EXPORT const unsigned char BBLAccessibilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBLAccessibility/PublicHeader.h>
+
+
