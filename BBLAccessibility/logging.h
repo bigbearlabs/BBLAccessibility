03f68e8c8efd4a551bb60e12902100f2fd1e37d0
Andy Park <andy@bigbearlabs.com>
2017-05-07 01:13:08 +0200

    refine logging choices.

--- BBLAccessibility/logging.h
+++ BBLAccessibility/logging.h
@@ -9,8 +9,10 @@
   //#define   __log(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__);
   // DISABLED until we can do base builds on 10.12...
 
-  //#define   __log(...) NSLog(@__VA_ARGS__);
+// uncomment to do primitive logging.
+//  #define   __log(...) NSLog(@__VA_ARGS__);
 
+  // impl the log convension in terms of pre 'os_log' api.
   #define __log(...) asl_NSLog(NULL, NULL, ASL_LEVEL_INFO, __VA_ARGS__);
 
   // credit: Peter Hosey.
