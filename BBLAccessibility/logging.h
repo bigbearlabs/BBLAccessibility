d7592efaa949bb5d1a007193b4860e4bbc7ee1ab
Andy Park <andy@bigbearlabs.com>
2017-06-30 12:38:27 +0200

    tweak logging.

--- BBLAccessibility/logging.h
+++ BBLAccessibility/logging.h
@@ -1,4 +1,4 @@
-//#import <os/log.h>
+#import <os/log.h>
 #import <asl.h>
 
 #ifndef logging_h
@@ -6,17 +6,23 @@
 
 #define logging_h
 
-  //#define   __log(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__);
-  // DISABLED until we can do base builds on 10.12...
+  // opt1. logging based on unified logging API.
+  // requires 10.12 or higher.
+  #define   __log(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__);
 
-// uncomment to do primitive logging.
+// // opt2.
+// // uncomment to do primitive logging.
 //  #define   __log(...) NSLog(@__VA_ARGS__);
 
-  // impl the log convension in terms of pre 'os_log' api.
-  #define __log(...) asl_NSLog(NULL, NULL, ASL_LEVEL_INFO, __VA_ARGS__);
+//  // opt3.
+//  // impl the log convension in terms of pre 'os_log' api.
+//  #define __log(...) asl_NSLog(NULL, NULL, ASL_LEVEL_INFO, __VA_ARGS__);
+//
+//  // credit: Peter Hosey.
+//  #define asl_NSLog(client, msg, level, format, ...) asl_log(client, msg, level, "%s", [[NSString stringWithFormat:@format, ##__VA_ARGS__] UTF8String])
 
-  // credit: Peter Hosey.
-  #define asl_NSLog(client, msg, level, format, ...) asl_log(client, msg, level, "%s", [[NSString stringWithFormat:@format, ##__VA_ARGS__] UTF8String])
+//  // opt4. silent.
+//  #define __log(...) ;
 
 #endif /* logging_h */
 
