b1742bc00bbea7a23e10e7d40102d3fbdceced9f
Andy Park <andy@bigbearlabs.com>
2017-06-30 12:35:14 +0200

    util function.

---
+++ BBLAccessibilitySwift/Silica-ext.swift
@@ -0,0 +1,28 @@
+//
+//  Silica-ext.swift
+//  BBLAccessibility
+//
+//  Created by ilo on 27/06/2017.
+//  Copyright © 2017 Big Bear Labs. All rights reserved.
+//
+
+import Foundation
+import Silica
+
+
+
+extension SIApplication {
+  
+  static public func application(bundleId: String) -> SIApplication? {
+    if let app = NSRunningApplication.runningApplications(withBundleIdentifier: bundleId).last {
+      return SIApplication(runningApplication: app)
+    }
+    else {
+      return nil
+    }
+  }
+  
+}
+
+
+
