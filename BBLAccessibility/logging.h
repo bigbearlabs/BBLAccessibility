48af25353a60aa5f004f0e65b8bb71351fa9054a
Andy Park <andy@bigbearlabs.com>
2017-11-27 13:42:49 +0100

    MASS-COMMIT begin auditing for objc inference.

--- BBLAccessibility/logging.h
+++ BBLAccessibility/logging.h
@@ -6,13 +6,25 @@
 
 #define logging_h
 
-  // opt1. logging based on unified logging API.
-  // requires 10.12 or higher.
-  #define   __log(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__);
+#ifndef BBL_LOG_AX
 
-// // opt2.
-// // uncomment to do primitive logging.
-//  #define   __log(...) NSLog(@__VA_ARGS__);
+  // opt4. silent.
+  #define __log(...) ;
+
+#else
+
+  #ifdef __AVAILABILITY_INTERNAL__MAC_10_12
+
+    // opt1. logging based on unified logging API.
+    // requires 10.12 or higher.
+
+    #define   __log(...) os_log_info(OS_LOG_DEFAULT, __VA_ARGS__);
+
+  #else
+  // // opt2.
+  // // uncomment to perform primitive logging.
+  //  #define   __log(...) NSLog(@__VA_ARGS__);
+  #endif
 
 //  // opt3.
 //  // impl the log convension in terms of pre 'os_log' api.
@@ -21,8 +33,7 @@
 //  // credit: Peter Hosey.
 //  #define asl_NSLog(client, msg, level, format, ...) asl_log(client, msg, level, "%s", [[NSString stringWithFormat:@format, ##__VA_ARGS__] UTF8String])
 
-//  // opt4. silent.
-//  #define __log(...) ;
+#endif
 
 #endif /* logging_h */
 
