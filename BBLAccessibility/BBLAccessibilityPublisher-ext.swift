03f97f37e3ee79dc81055a31863a6144e33ac312
Andy Park <andy@bigbearlabs.com>
2021-06-30 16:46:39 +0900

    tidy.

--- BBLAccessibility/BBLAccessibilityPublisher-ext.swift
+++ BBLAccessibility/BBLAccessibilityPublisher-ext.swift
@@ -2,76 +2,6 @@ import Combine
 
 
 public extension BBLAccessibilityPublisher {
-
-  typealias State = [Int : [CGWindowInfo]]
-
-  
-  func withCurrentState(completionHandler: (State) -> Void) {
-    let cgWindows = CGWindowInfo.query(scope: .onScreen, otherOptions: [.excludeDesktopElements])
-    // TODO scope needs to change to 'all' in order for dock to show up during mission control activation.
-    let onScreenCgWindows = cgWindows
-      .filter { $0.isInActiveSpace }
-      .filter {
-        // exclude pids for status menu items or dock
-        ![
-          NSWindow.Level.mainMenu.rawValue,
-          NSWindow.Level.statusBar.rawValue,
-//          NSWindow.Level.dock.rawValue,  // deprecated in 10.13
-        ].contains($0.windowLayer)
-      }
-
-    // DISABLED further filtering based on ax queries can spin, and produces unstable results depending on ax target app responsiveness.
-//    //  reject windows not seen by ax api, e.g. transparent windows.
-//    let pidsForCgWindows = onScreenCgWindows.map { $0.pid }.uniqueValues
-//
-//    // filter down to windows reported on-screen by ax api.
-//    var onScreenAxWindowIds: [CGWindowID] = []
-//    let g = DispatchGroup()
-//    let l = NSLock()
-//    for pid in pidsForCgWindows {
-//      g.enter()
-//      windows(pid: pid) { windows in
-//        l.lock()
-//        onScreenAxWindowIds.append(contentsOf: windows.map { $0.windowID })
-//        l.unlock()
-//        g.leave()
-//      }
-//    }
-//
-//    _ = g.wait(timeout: .now() + 0.2) // HARDCODED
-
-//    let axFilteredCgWindows = onScreenCgWindows.filter { window in
-//      onScreenAxWindowIds.contains(CGWindowID(window.windowId.windowNumber)!)
-//    }
-    
-    let axFilteredCgWindows = onScreenCgWindows
-    // group by screen based on frame
-    
-    let screens = NSScreen.screens
-    let windowInfoListsByScreenId = Dictionary(
-      grouping: axFilteredCgWindows.map { windowInfo -> (Int, CGWindowInfo) in
-        for (i, screen) in screens.enumerated() {
-          if windowInfo.frame.intersects(screen.frame) {
-            return (i, windowInfo)
-          }
-        }
-        // no intersection; assume belonging to first screen.
-        return (0, windowInfo)
-      },
-      by: { screenId, _ in screenId }
-    )
-      .mapValues {
-        $0.map { screenId, windowInfoLists in
-          windowInfoLists
-        }
-      }
-    
-    completionHandler(windowInfoListsByScreenId)
-    
-  }
-  
-
-  // MARK: -
   
   func windows(pid: pid_t, completionHandler: @escaping ([SIWindow]) -> Void) {
     siQuery(pid: pid) { siApp in
@@ -189,18 +119,23 @@ class RunningApplicationsBookkeeper {
   var runningApplications = NSWorkspace.shared.runningApplications {
     didSet {
       let newApps = Set(runningApplications).subtracting(oldValue)
-      for app in newApps {
-        let sendOnFinishedLaunching = app.publisher(for: \.isFinishedLaunching, options: [.initial, .new])
-          .filter { $0 == true }
-          .map { (app, $0) }
-          .sink { app, isFinishedLaunching in
-            appEventPublisher.send(.launched(app))
-            self.finishedLaunchingSubsByPid[app.processIdentifier] = nil
-          }
-        self.finishedLaunchingSubsByPid[app.processIdentifier] = sendOnFinishedLaunching
-        //  most app will signal readiness via isFinishedLaunching.
-        // what of apps that never do? warn after delay?
-      }
+      
+      let newAppSubs = Dictionary(uniqueKeysWithValues: newApps.map { app in
+        (
+          app.processIdentifier,
+          app.publisher(for: \.isFinishedLaunching, options: [.initial, .new])
+            .filter { $0 == true }
+            .map { (app, $0) }
+            .sink { app, isFinishedLaunching in
+              appEventPublisher.send(.launched(app))
+              self.finishedLaunchingSubsByPid[app.processIdentifier] = nil
+            }
+        )
+      })
+      
+      self.finishedLaunchingSubsByPid.merge(newAppSubs) { $1 }
+      //  most app will signal readiness via isFinishedLaunching.
+      // what of apps that never do? warn after delay?
       
       let terminatedApps = Set(oldValue).subtracting(runningApplications)
       for app in terminatedApps {
