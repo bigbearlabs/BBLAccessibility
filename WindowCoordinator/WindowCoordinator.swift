9dc138c81fce3dedfafd43f652d3e80b392fef8c
Andy Park <andy@bigbearlabs.com>
2021-07-10 00:07:12 +0900

    WindowCoordinator: internalise coordinator queue

--- WindowCoordinator/WindowCoordinator.swift
+++ WindowCoordinator/WindowCoordinator.swift
@@ -8,37 +8,43 @@ public class WindowCoordinator {
   
   public init() {}
   
-  
   public func position(
     framesByWindowNumber: [UInt32 : CGRect],
     raise: Bool = false,
-    activate windowNumberToFocus: UInt32? = nil) {
-    for (windowNumber, frame) in framesByWindowNumber {
-      
-      if let window = SIWindow.for(windowNumber: windowNumber) {
-        if frame == .zero {
-          print("👺 window \(windowNumber) is given a zero frame; will not set.")
-        }
-        else if window.frame() != frame {
-          window.setFrame(frame)
-        }
+    activate windowNumberToFocus: UInt32? = nil,
+    queue: DispatchQueue = coordinatorQueue
+  ) {
+
+    // TODO animations
+    coordinatorQueue.async {
+
+      for (windowNumber, frame) in framesByWindowNumber {
         
-        if raise {
-          if windowNumberToFocus != nil
-            && windowNumber == windowNumberToFocus {
-            // don't raise since we will focus later
-          } else {
-            self.raise(windowNumber: windowNumber)
+        if let window = SIWindow.for(windowNumber: windowNumber) {
+          if frame == .zero {
+            print("👺 window \(windowNumber) is given a zero frame; will not set.")
+          }
+          else if window.frame() != frame {
+            window.setFrame(frame)
+          }
+          
+          if raise {
+            if windowNumberToFocus != nil
+              && windowNumber == windowNumberToFocus {
+              // don't raise since we will focus later
+            } else {
+              self.raise(windowNumber: windowNumber)
+            }
           }
         }
       }
-    }
-    
-    if let n = windowNumberToFocus {
       
-      self.focus(windowNumber: n)
-    }
+      if let n = windowNumberToFocus {
+        
+        self.focus(windowNumber: n)
+      }
 
+    }
   }
   
   public func focus(windowNumber: UInt32) {
@@ -91,4 +97,6 @@ extension SIWindow {
   
 }
 
+
+public let coordinatorQueue = DispatchQueue.global(qos: .userInteractive)
 
