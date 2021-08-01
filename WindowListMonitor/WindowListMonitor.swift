039e4d9895050e703b585d8563587abfd008df5b
Andy Park <andy@bigbearlabs.com>
2021-08-02 00:43:22 +0900

    tidy.

--- WindowListMonitor/WindowListMonitor.swift
+++ WindowListMonitor/WindowListMonitor.swift
@@ -44,6 +44,9 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
   }
   
   
+  var windowsByPid: [pid_t : [SIWindow]] = [:]
+
+  
   let handler: (Event) -> Void
   
   let handlerQueue: DispatchQueue
@@ -60,16 +63,9 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
   public func observeEvents() {
     self.watchWindows()
     
-    for app in applicationsToObserve {
-      let pid = app.processIdentifier
-      if let siApp = appElement(forProcessIdentifier: pid) {
-        for window in siApp.uncachedWindows {
-          track(window: window, pid: pid)
-        }
-      }
-    }
+    trackObservableApplicationsExistingWindows()
   }
-
+  
     
     
 //    self.registerForNotification()
@@ -110,70 +106,6 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
 ////    self.unwatchWindows2()
 //  }
   
-  // bookkeep this window.
-  // TODO synchronise.
-  func track(window: SIWindow, pid: pid_t) {
-    if !self.windowsByPid[pid, default: []].contains(window) {
-      
-      if let siApp = self.appElement(forProcessIdentifier: pid) {
-        let result = siApp.observeAxNotification(kAXUIElementDestroyedNotification as CFString, with: window)
-        print("!! watching \(window) for destruction. result:\(result.rawValue)")
-      }
-      else {
-        print("!!!! couldn't find SIApplication for pid:\(pid); can't track window for destruction.")
-      }
-
-      self.windowsByPid[pid, default:[]].append(window)
-    }
-    
-//    func isTracked(tabGroup: SITabGroup) -> Bool {
-//      tabGroupsByWindowId.values.contains { $0.axElementRef == tabGroup.axElementRef}
-//    }
-//  
-//    func track(tabGroup: SITabGroup, window: SIWindow) {
-//      tabGroupsByWindowId[window.windowID] = tabGroup
-//      
-//      let siApp = appElement(forProcessIdentifier: tabGroup.processIdentifier())
-//      siApp?.observeAxNotification(<#T##notification: CFString##CFString#>, with: <#T##SIAccessibilityElement#>)
-//    }
-//
-//    if let tabGroup = window.tabGroup,
-//       !isTracked(tabGroup: tabGroup) {
-//      track(tabGroup: tabGroup, window: window)
-//    }
-  }
-
-  
-  func untrack(window: SIWindow, pid: pid_t) {
-    self.windowsByPid[pid]?.removeAll { $0.axElementRef == window.axElementRef }
-  }
-  
-  func trackedWindow(element: SIAccessibilityElement) -> SIWindow? {
-    windowsByPid
-      .flatMap { $0.value }
-      .first { $0.axElementRef == element.axElementRef }
-  }
-  
-  func snapshot(tabGroupElement: SIAccessibilityElement, window: SIWindow) {
-    let tabGroup = SITabGroup(axElement: tabGroupElement.axElementRef)
-    let tabs = tabGroup.tabs
-    if tabContentsByWindow[window.axElementRef] != tabs {
-      tabContentsByWindow[window.axElementRef] = tabs
-      print("!! took tabs snapshot of \(window)")
-    }
-    
-  }
-  
-  // TODO initially populate as much as possible.
-  var windowsByPid: [pid_t : [SIWindow]] = [:]
-  
-  var tabContentsByWindow: [AXUIElement : [SITabGroup.Tab]] = [:]
-  
-  var windowsWithoutTabGroups: [SIWindow] = []
-  var windowsByTabGroups: [AXUIElement: [SIWindow]] = [:]
-
-
-  // TODO track window coming into radar.
   
   override public func updateAccessibilityInfo(for siElement: SIAccessibilityElement, axNotification: CFString, forceUpdate: Bool) {
 //    super.updateAccessibilityInfo(for: siElement, axNotification: axNotification, forceUpdate: forceUpdate)
@@ -203,70 +135,11 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
       }
       
       let window = SIWindow(for: siElement)
-      let windowNumber = window.windowID
       let pid = siElement.processIdentifier()
+      
       track(window: window, pid: pid)
 
-      let tabGroup = window.tabGroup
-      
-      handle(.focused(windowNumber: windowNumber, tabGroup: tabGroup))
-      
-      func bookkeep(_ window: SIWindow, _ tabGroup: SITabGroup?) {
-        func prune(window: SIWindow) {
-          windowsByTabGroups = windowsByTabGroups.mapValues {
-            var ws = $0
-            ws.removeAll { $0 == window }
-            return ws
-          }
-          windowsWithoutTabGroups.removeAll { $0 == window }
-        }
-
-        if let tabGroup = tabGroup {
-          // first prune.
-          prune(window: window)
-          // then insert.
-          windowsByTabGroups[tabGroup.axElementRef, default: []].append(window)
-          let tabWindows = windowsByTabGroups[tabGroup.axElementRef] ?? []
-          print("!! tab group has windows: \(tabWindows)")
-        } else {
-          assert(window.isVisible())
-          prune(window: window)
-          windowsWithoutTabGroups.append(window)
-        }
-      }
-      
-      func tabContext(
-        window: SIWindow) -> TabContext? {
-        // state
-        var visibleWindowsByPid: [pid_t : [SIWindow]] = [:]
-        
-        let pid = window.processIdentifier()
-        let space = spaceForWindow(window.windowID)
-        let priorWindows = visibleWindowsByPid[pid] ?? []
-        let currentWindows = SIApplication(forProcessIdentifier: pid).windows
-        visibleWindowsByPid[pid] = currentWindows
-        // pre-condition: no prior windows have been closed.
-        let disappearedWids =
-          Set(priorWindows.map { $0.windowID })
-          .subtracting(currentWindows.map { $0.windowID })
-          .filter { spaceForWindow($0) == space }
-          
-        if disappearedWids.count == 0 {
-          return nil
-        } else if disappearedWids.count > 1 {
-          return nil
-        }
-        
-        if let priorTabOwner = priorWindows.first { $0.windowID == disappearedWids.first! } {
-          return TabContext(priorTabOwner: priorTabOwner)
-        }
-        
-        return nil
-      }
-      
-      struct TabContext {
-        let priorTabOwner: SIWindow
-      }
+      handle(.focused(windowNumber: window.windowID, tabGroup: window.tabGroup))
       
     case kAXTitleChangedNotification:
       let window = SIWindow(for: siElement)
@@ -277,23 +150,18 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
     case kAXApplicationActivatedNotification:
       let pid = siElement.processIdentifier()
       
-      // log just activated notif to ensure we're listening.
-      print("activated pid:\(pid) (\(siElement.title() ?? "?"))")
-      
       let window = SIWindow(for: siElement.focused())
       track(window: window, pid: pid)
 
-      focusedWindow(pid: pid) { [unowned self] window in
-        print("activated pid:\(pid) (\(siElement.title() ?? "?")), ax reports focused window \(window?.windowID ?? kCGNullWindowID)")
-        handle(.activated(pid: pid, focusedWindowNumber: window?.windowID, tabGroup: window?.tabGroup))
-      }
+      print("activated pid:\(pid) (\(siElement.title() ?? "?")), ax reports focused window \(window.windowID)")
+      handle(.activated(pid: pid, focusedWindowNumber: window.windowID, tabGroup: window.tabGroup))
 
-    case kAXApplicationDeactivatedNotification:
-      focusedWindow() { [unowned self] focusedWindow in
-        if let focusedWindow = focusedWindow {
-          handle(.activated(pid: focusedWindow.processIdentifier(), focusedWindowNumber: focusedWindow.windowID, tabGroup: focusedWindow.tabGroup))
-        }
-      }
+//    case kAXApplicationDeactivatedNotification:
+//      focusedWindow() { [unowned self] focusedWindow in
+//        if let focusedWindow = focusedWindow {
+//          handle(.activated(pid: focusedWindow.processIdentifier(), focusedWindowNumber: focusedWindow.windowID, tabGroup: focusedWindow.tabGroup))
+//        }
+//      }
 
     case kAXWindowMovedNotification,
          kAXWindowResizedNotification:
@@ -404,8 +272,8 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
     return true
   }
   
-  lazy var myPid = NSRunningApplication.current.processIdentifier
 
+  
   lazy var excludedBundleIdSubstrings: [String] = {
     return (UserDefaults.standard.stringArray(forKey: "excludedBundleIdPatterns")  ?? [])
       + [
@@ -460,9 +328,62 @@ public class WindowListMonitor: BBLAccessibilityPublisher {
     ]
   }
 
-  public override func handleAxObservationResults(_ axResults: [NSNumber], for application: NSRunningApplication) {
-    // observe on the next opportunity.
-    // when app activated?
+
+  // MARK: -
+  
+  // bookkeep this window.
+  // TODO synchronise.
+  func track(window: SIWindow, pid: pid_t) {
+    if !self.windowsByPid[pid, default: []].contains(window) {
+      
+      if let siApp = self.appElement(forProcessIdentifier: pid) {
+        let result = siApp.observeAxNotification(kAXUIElementDestroyedNotification as CFString, with: window)
+        print("!! watching \(window) for destruction. result:\(result.rawValue)")
+      }
+      else {
+        print("!!!! couldn't find SIApplication for pid:\(pid); can't track window for destruction.")
+      }
+
+      self.windowsByPid[pid, default:[]].append(window)
+    }
+    
+//    func isTracked(tabGroup: SITabGroup) -> Bool {
+//      tabGroupsByWindowId.values.contains { $0.axElementRef == tabGroup.axElementRef}
+//    }
+//
+//    func track(tabGroup: SITabGroup, window: SIWindow) {
+//      tabGroupsByWindowId[window.windowID] = tabGroup
+//
+//      let siApp = appElement(forProcessIdentifier: tabGroup.processIdentifier())
+//      siApp?.observeAxNotification(<#T##notification: CFString##CFString#>, with: <#T##SIAccessibilityElement#>)
+//    }
+//
+//    if let tabGroup = window.tabGroup,
+//       !isTracked(tabGroup: tabGroup) {
+//      track(tabGroup: tabGroup, window: window)
+//    }
+  }
+
+  
+  func untrack(window: SIWindow, pid: pid_t) {
+    self.windowsByPid[pid]?.removeAll { $0.axElementRef == window.axElementRef }
+  }
+  
+  func trackedWindow(element: SIAccessibilityElement) -> SIWindow? {
+    windowsByPid
+      .flatMap { $0.value }
+      .first { $0.axElementRef == element.axElementRef }
+  }
+  
+  func trackObservableApplicationsExistingWindows() {
+    for app in applicationsToObserve {
+      let pid = app.processIdentifier
+      if let siApp = appElement(forProcessIdentifier: pid) {
+        for window in siApp.uncachedWindows {
+          track(window: window, pid: pid)
+        }
+      }
+    }
   }
 
 }
